7715604e84db2f88d0862fc884f74057fb9193b5
Muhammad Rayhan <saviourcat@gmail.com>
2019-03-12 13:06:42 +0700

    First commit

---
+++ 01-Hello/src/main/java/com/raysoft/springboot/Hello/controller/HelloController.java
@@ -0,0 +1,31 @@
+package com.raysoft.springboot.Hello.controller;
+
+import com.raysoft.springboot.Hello.entity.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello-customer", produces= MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Object> getCustomer(@RequestParam(value="id") Long id) {
+        if (id == null) {
+            return new ResponseEntity<Object>("Parameter ID not supplied", HttpStatus.BAD_REQUEST);
+        }
+        Customer c = new Customer();
+        ResponseEntity<Object> resp = null;
+
+        if (id == 0) {
+            c.setName("Customer with ID = 0");
+            resp = new ResponseEntity<Object>(c, HttpStatus.OK);
+        } else {
+            c.setName("#itmaker");
+            resp = new ResponseEntity<Object>(c, HttpStatus.OK);
+        }
+        return resp;
+    }
+}
---
+++ 01-Hello/src/main/java/com/raysoft/springboot/Hello/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.raysoft.springboot.Hello.entity;
+
+public class Customer {
+    private long id;
+    private String name;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
