f42ba34d42e2dbad85b98308aaabe301534715c6
Muhammad Rayhan <saviourcat@gmail.com>
2019-03-25 17:10:59 +0700

    Add Swagger capability

--- 01-Hello/src/main/java/com/raysoft/springboot/Hello/controller/HelloController.java
+++ 01-Hello/src/main/java/com/raysoft/springboot/Hello/controller/HelloController.java
@@ -4,14 +4,14 @@ import com.raysoft.springboot.Hello.entity.Customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/hello-customer", produces= MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/hello-customer", produces= MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Object> getCustomer(@RequestParam(value="id") Long id) {
         if (id == null) {
             return new ResponseEntity<Object>("Parameter ID not supplied", HttpStatus.BAD_REQUEST);
